44711c2166696c5a9dc355e48206b08685a81905
heyshreenanda <1ms24cs416@msrit.edu>
2026-07-28 21:20:27 +0530

    So 2nd commit of the day of string practice

--- stringPractice.py
+++ stringPractice.py
@@ -14,13 +14,32 @@ print(reverse)'''
 
 #count vowels
 
-str = "Shreenanda"
+'''str = "Shreenanda"
 str = str.upper()
 vowels = "AEIOU"
 count = 0
 for ch in str:
     if ch in vowels:
         count += 1
-print(f"Total number of vowels are : {count}")
+print(f"Total number of vowels are : {count}")'''
 
-    
+
+#count uppercase, lower case, numbers and special characters 
+
+text = "Hellow123@2025"
+num_count = 0
+char_count = 0
+lower_count = 0
+upper_count = 0
+
+for ch in text:
+    ascii_value = ord(ch)
+    if ascii_value >= 65 and ascii_value <= 90:
+        upper_count += 1
+    elif ascii_value >= 97 and ascii_value <= 122:
+        lower_count += 1
+    elif ascii_value >= 48 and ascii_value <= 57:
+        num_count += 1
+    else: char_count += 1
+
+print(f"Total counts are \n Upper case: {upper_count} \n Lower case: {lower_count} \n Digits :{num_count}\n Special characters: {char_count}")
