64617592f5df7b675e464b284e262704e06b3064
heyshreenanda <1ms24cs416@msrit.edu>
2026-07-29 21:30:59 +0530

    Updated leet code problem

--- contains_dupl.py
+++ contains_dupl.py
@@ -1,3 +1,9 @@
+#217. Contains Duplicate
+
+#Given an integer array nums, return true if any value appears at least twice in the array, and return'''' false if every element is distinct
+
+#Time complexity is O(n) and Space Complexity is O(n)
+
 class Solution:
     def contains_dupl(self,nums):
         freq = {}
